6b17e994b5cfdd7224cdff6ae32d13286fd16ba9
helinjiang <onehlj@qq.com>
2020-08-28 12:02:29 +0800

    feat: 重构 detail

---
+++ src/pages/detail/components/mockstar-sample/index.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { Button, Card, Col, Input, Row, Tree } from 'antd';
+
+import { createFolderTree, downloadSampleCode } from '../../business';
+
+import './index.less';
+
+export default class MockStarSample extends Component {
+  constructor(...props) {
+    super(...props);
+
+    this.state = {
+      selectedTreeKey: 'mockModulesDebugIndexJs',
+    };
+  }
+
+  handleSelectTree = (keys, event) => {
+    console.log('Select Tree Node', keys, event);
+
+    this.setState({
+      selectedTreeKey: keys[0],
+    });
+  };
+
+  handleDownload = (treeNode) => {
+    if (!treeNode) {
+      console.log('treeNode is undefined!');
+      return;
+    }
+
+    if (!treeNode.isLeaf) {
+      console.log('treeNode is not isLeaf!');
+      return;
+    }
+
+    downloadSampleCode(treeNode.content, treeNode.title);
+  };
+
+  render() {
+    const { selectedTreeKey } = this.state;
+    const { currentNetwork } = this.props;
+
+    const { treeData, treeNodeMap } = createFolderTree(currentNetwork);
+    const treeNode = treeNodeMap[selectedTreeKey];
+
+    return (
+      <div className="mockstar-sample">
+        <Row>
+          <Col span={8}>
+            <Tree.DirectoryTree
+              defaultExpandAll
+              defaultSelectedKeys={[selectedTreeKey]}
+              onSelect={this.handleSelectTree}
+              treeData={treeData}
+            />
+          </Col>
+
+          <Col span={16}>
+            <Card title={`${(treeNode && treeNode.path) || '请选择'}`}
+                  extra={<Button type="primary" onClick={() => {
+                    this.handleDownload(treeNode);
+                  }}>单独下载该文件</Button>}
+            >
+              <Input.TextArea
+                value={treeNode && treeNode.content}
+                placeholder="点击左侧目录树查看文件内容"
+                autoSize={{ minRows: 3 }}
+              />
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,54 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Button, Card, Col, Descriptions, Input, PageHeader, Row, Tabs, Tag, Tree } from 'antd';
-
-import { createFolderTree, downloadSampleCode } from './business';
+import { Card, Descriptions, Input, PageHeader, Tabs, Tag } from 'antd';
 
 import MockStarCms from './components/mockstar-cms';
+import MockStarSample from './components/mockstar-sample';
 
 import './index.less';
 
 class PageDetail extends Component {
-  constructor(...props) {
-    super(...props);
-
-    this.state = {
-      selectedTreeKey: 'mockModulesDebugIndexJs',
-    };
-  }
-
   gotoHome = () => {
     this.props.history.push(`/`);
-  };
-
-  handleSelectTree = (keys, event) => {
-    console.log('Trigger Select', keys, event);
-
-    this.setState({
-      selectedTreeKey: keys[0],
-    });
-  };
-
-  handleLoadTree = (keys, event) => {
-    console.log('Trigger success_json_file.json', keys, event);
-
-    this.setState({
-      selectedTreeKey: keys[0],
-    });
-  };
-
-  handleDownload = (treeNode) => {
-    if (!treeNode) {
-      console.log('treeNode is undefined!');
-      return;
-    }
-
-    if (!treeNode.isLeaf) {
-      console.log('treeNode is not isLeaf!');
-      return;
-    }
-
-    downloadSampleCode(treeNode.content, treeNode.title);
   };
 
   getCurrentNetwork = () => {
@@ -60,11 +21,8 @@ class PageDetail extends Component {
 
   render() {
     const id = this.props.match.params.id;
-    const { selectedTreeKey } = this.state;
-    const currentNetwork = this.getCurrentNetwork();
 
-    const { treeData, treeNodeMap } = createFolderTree(currentNetwork);
-    const treeNode = treeNodeMap[selectedTreeKey];
+    const currentNetwork = this.getCurrentNetwork();
 
     return (
       <div className="page-detail">
@@ -96,32 +54,9 @@ class PageDetail extends Component {
               />
             </Card>
           </Tabs.TabPane>
-          <Tabs.TabPane tab="mockstar样例代码" key="2">
-            <Row>
-              <Col span={8}>
-                <Tree.DirectoryTree
-                  defaultExpandAll
-                  defaultSelectedKeys={[selectedTreeKey]}
-                  onSelect={this.handleSelectTree}
-                  onLoad={this.handleLoadTree}
-                  treeData={treeData}
-                />
-              </Col>
 
-              <Col span={16}>
-                <Card title={`${(treeNode && treeNode.path) || '请选择'}`}
-                      extra={<Button type="primary" onClick={() => {
-                        this.handleDownload(treeNode);
-                      }}>下载该文件</Button>}
-                >
-                  <Input.TextArea
-                    value={treeNode && treeNode.content}
-                    placeholder="点击左侧目录树查看文件内容"
-                    autoSize={{ minRows: 3 }}
-                  />
-                </Card>
-              </Col>
-            </Row>
+          <Tabs.TabPane tab="mockstar样例代码" key="2">
+            <MockStarSample currentNetwork={currentNetwork} />
           </Tabs.TabPane>
 
           {
